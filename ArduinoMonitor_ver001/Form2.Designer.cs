248ecc1e086928f1be09c4c940748fcd013beacb
tomoki sugiura <sugiuratomoki7@gmail.com>
2022-01-15 07:04:21 +0900

    白鍵と黒鍵を表示できるようにした

--- ArduinoMonitor_ver001/Form2.Designer.cs
+++ ArduinoMonitor_ver001/Form2.Designer.cs
@@ -28,19 +28,153 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel_Board = new System.Windows.Forms.Panel();
+            this.panel_C = new System.Windows.Forms.Panel();
+            this.panel_D = new System.Windows.Forms.Panel();
+            this.panel_E = new System.Windows.Forms.Panel();
+            this.panel_F = new System.Windows.Forms.Panel();
+            this.panel_G = new System.Windows.Forms.Panel();
+            this.panel_A = new System.Windows.Forms.Panel();
+            this.panel_B = new System.Windows.Forms.Panel();
+            this.panel_Csharp = new System.Windows.Forms.Panel();
+            this.panel_Dsharp = new System.Windows.Forms.Panel();
+            this.panel_Fsharp = new System.Windows.Forms.Panel();
+            this.panel_Gsharp = new System.Windows.Forms.Panel();
+            this.panel_Asharp = new System.Windows.Forms.Panel();
+            this.panel_Board.SuspendLayout();
             this.SuspendLayout();
             // 
-            // panel1
+            // panel_Board
             // 
-            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.panel1.Cursor = System.Windows.Forms.Cursors.Default;
-            this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(492, 263);
-            this.panel1.TabIndex = 0;
+            this.panel_Board.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.panel_Board.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel_Board.Controls.Add(this.panel_Asharp);
+            this.panel_Board.Controls.Add(this.panel_Gsharp);
+            this.panel_Board.Controls.Add(this.panel_Fsharp);
+            this.panel_Board.Controls.Add(this.panel_Dsharp);
+            this.panel_Board.Controls.Add(this.panel_Csharp);
+            this.panel_Board.Controls.Add(this.panel_B);
+            this.panel_Board.Controls.Add(this.panel_A);
+            this.panel_Board.Controls.Add(this.panel_G);
+            this.panel_Board.Controls.Add(this.panel_F);
+            this.panel_Board.Controls.Add(this.panel_E);
+            this.panel_Board.Controls.Add(this.panel_D);
+            this.panel_Board.Controls.Add(this.panel_C);
+            this.panel_Board.Cursor = System.Windows.Forms.Cursors.Default;
+            this.panel_Board.Location = new System.Drawing.Point(0, 0);
+            this.panel_Board.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panel_Board.Name = "panel_Board";
+            this.panel_Board.Size = new System.Drawing.Size(492, 263);
+            this.panel_Board.TabIndex = 0;
+            // 
+            // panel_C
+            // 
+            this.panel_C.BackColor = System.Drawing.Color.White;
+            this.panel_C.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel_C.Location = new System.Drawing.Point(57, 87);
+            this.panel_C.Name = "panel_C";
+            this.panel_C.Size = new System.Drawing.Size(47, 114);
+            this.panel_C.TabIndex = 0;
+            // 
+            // panel_D
+            // 
+            this.panel_D.BackColor = System.Drawing.Color.White;
+            this.panel_D.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel_D.Location = new System.Drawing.Point(121, 87);
+            this.panel_D.Name = "panel_D";
+            this.panel_D.Size = new System.Drawing.Size(47, 114);
+            this.panel_D.TabIndex = 0;
+            // 
+            // panel_E
+            // 
+            this.panel_E.BackColor = System.Drawing.Color.White;
+            this.panel_E.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel_E.Location = new System.Drawing.Point(185, 87);
+            this.panel_E.Name = "panel_E";
+            this.panel_E.Size = new System.Drawing.Size(47, 114);
+            this.panel_E.TabIndex = 0;
+            // 
+            // panel_F
+            // 
+            this.panel_F.BackColor = System.Drawing.Color.White;
+            this.panel_F.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel_F.Location = new System.Drawing.Point(254, 87);
+            this.panel_F.Name = "panel_F";
+            this.panel_F.Size = new System.Drawing.Size(47, 114);
+            this.panel_F.TabIndex = 0;
+            // 
+            // panel_G
+            // 
+            this.panel_G.BackColor = System.Drawing.Color.White;
+            this.panel_G.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel_G.Location = new System.Drawing.Point(320, 87);
+            this.panel_G.Name = "panel_G";
+            this.panel_G.Size = new System.Drawing.Size(47, 114);
+            this.panel_G.TabIndex = 0;
+            // 
+            // panel_A
+            // 
+            this.panel_A.BackColor = System.Drawing.Color.White;
+            this.panel_A.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel_A.Location = new System.Drawing.Point(385, 87);
+            this.panel_A.Name = "panel_A";
+            this.panel_A.Size = new System.Drawing.Size(47, 114);
+            this.panel_A.TabIndex = 0;
+            // 
+            // panel_B
+            // 
+            this.panel_B.BackColor = System.Drawing.Color.White;
+            this.panel_B.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel_B.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.panel_B.Location = new System.Drawing.Point(456, 87);
+            this.panel_B.Name = "panel_B";
+            this.panel_B.Size = new System.Drawing.Size(47, 114);
+            this.panel_B.TabIndex = 0;
+            // 
+            // panel_Csharp
+            // 
+            this.panel_Csharp.BackColor = System.Drawing.Color.Black;
+            this.panel_Csharp.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel_Csharp.Location = new System.Drawing.Point(85, 42);
+            this.panel_Csharp.Name = "panel_Csharp";
+            this.panel_Csharp.Size = new System.Drawing.Size(51, 139);
+            this.panel_Csharp.TabIndex = 1;
+            // 
+            // panel_Dsharp
+            // 
+            this.panel_Dsharp.BackColor = System.Drawing.Color.Black;
+            this.panel_Dsharp.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel_Dsharp.Location = new System.Drawing.Point(142, 42);
+            this.panel_Dsharp.Name = "panel_Dsharp";
+            this.panel_Dsharp.Size = new System.Drawing.Size(51, 139);
+            this.panel_Dsharp.TabIndex = 1;
+            // 
+            // panel_Fsharp
+            // 
+            this.panel_Fsharp.BackColor = System.Drawing.Color.Black;
+            this.panel_Fsharp.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel_Fsharp.Location = new System.Drawing.Point(199, 42);
+            this.panel_Fsharp.Name = "panel_Fsharp";
+            this.panel_Fsharp.Size = new System.Drawing.Size(51, 139);
+            this.panel_Fsharp.TabIndex = 1;
+            // 
+            // panel_Gsharp
+            // 
+            this.panel_Gsharp.BackColor = System.Drawing.Color.Black;
+            this.panel_Gsharp.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel_Gsharp.Location = new System.Drawing.Point(259, 42);
+            this.panel_Gsharp.Name = "panel_Gsharp";
+            this.panel_Gsharp.Size = new System.Drawing.Size(51, 139);
+            this.panel_Gsharp.TabIndex = 1;
+            // 
+            // panel_Asharp
+            // 
+            this.panel_Asharp.BackColor = System.Drawing.Color.Black;
+            this.panel_Asharp.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel_Asharp.Location = new System.Drawing.Point(320, 42);
+            this.panel_Asharp.Name = "panel_Asharp";
+            this.panel_Asharp.Size = new System.Drawing.Size(51, 139);
+            this.panel_Asharp.TabIndex = 1;
             // 
             // Form2
             // 
@@ -48,18 +182,31 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ButtonFace;
             this.ClientSize = new System.Drawing.Size(831, 435);
-            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel_Board);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form2";
             this.Text = "PianoMonitor";
             this.SizeChanged += new System.EventHandler(this.Form2_SizeChanged_1);
+            this.panel_Board.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel_Board;
+        private System.Windows.Forms.Panel panel_B;
+        private System.Windows.Forms.Panel panel_A;
+        private System.Windows.Forms.Panel panel_G;
+        private System.Windows.Forms.Panel panel_F;
+        private System.Windows.Forms.Panel panel_E;
+        private System.Windows.Forms.Panel panel_D;
+        private System.Windows.Forms.Panel panel_C;
+        private System.Windows.Forms.Panel panel_Asharp;
+        private System.Windows.Forms.Panel panel_Gsharp;
+        private System.Windows.Forms.Panel panel_Fsharp;
+        private System.Windows.Forms.Panel panel_Dsharp;
+        private System.Windows.Forms.Panel panel_Csharp;
     }
 }
--- ArduinoMonitor_ver001/Form2.cs
+++ ArduinoMonitor_ver001/Form2.cs
@@ -28,8 +28,125 @@ namespace ArduinoMonitor_ver001
         }
         private void Panel_Resize()
         {
-            panel1.Location = new Point(ClientSize.Width / 16, ClientSize.Height / 6);
-            panel1.Size = new Size(ClientSize.Width * 14 / 16, ClientSize.Height * 4 / 6);
+            int Form_horizontal_divid = 15;
+            int Form_vertical_divid = 12;
+            //ピアノ本体の位置とサイズ変更
+            panel_Board.Location = new Point(this.Width / Form_horizontal_divid, this.Height * 2 / Form_vertical_divid);
+            panel_Board.Size = new Size(this.Width * 13 / Form_horizontal_divid, this.Height * 8 / Form_vertical_divid);
+
+            //白鍵の位置とサイズ変更
+            int panel_Board_horizontal_divid = 9;
+            int panel_Board_vertical_divid = 10;
+            //C
+            panel_C.Location = new Point(
+                panel_Board.Width * ((panel_Board_horizontal_divid - 7) / 2) / panel_Board_horizontal_divid,
+                panel_Board.Height * 2 / panel_Board_vertical_divid
+                );
+            panel_C.Size = new Size(
+                panel_Board.Width / panel_Board_horizontal_divid,
+                panel_Board.Height * (panel_Board_vertical_divid - 3) / panel_Board_vertical_divid
+                );
+            //D
+            panel_D.Location = new Point(
+                panel_Board.Width * ((panel_Board_horizontal_divid - 7) / 2 + 1) / panel_Board_horizontal_divid, 
+                panel_Board.Height * 2 / panel_Board_vertical_divid
+                );
+            panel_D.Size = new Size(
+                panel_Board.Width / panel_Board_horizontal_divid, 
+                panel_Board.Height * (panel_Board_vertical_divid - 3) / panel_Board_vertical_divid
+                );
+            //E
+            panel_E.Location = new Point(
+                panel_Board.Width * ((panel_Board_horizontal_divid - 7) / 2 + 2) / panel_Board_horizontal_divid,
+                panel_Board.Height * 2 / panel_Board_vertical_divid
+                );
+            panel_E.Size = new Size(
+                panel_Board.Width / panel_Board_horizontal_divid,
+                panel_Board.Height * (panel_Board_vertical_divid - 3) / panel_Board_vertical_divid
+                );
+            //F
+            panel_F.Location = new Point(
+                panel_Board.Width * ((panel_Board_horizontal_divid - 7) / 2 + 3) / panel_Board_horizontal_divid,
+                panel_Board.Height * 2 / panel_Board_vertical_divid
+                );
+            panel_F.Size = new Size(
+                panel_Board.Width / panel_Board_horizontal_divid,
+                panel_Board.Height * (panel_Board_vertical_divid - 3) / panel_Board_vertical_divid
+                );
+            //G
+            panel_G.Location = new Point(
+                panel_Board.Width * ((panel_Board_horizontal_divid - 7) / 2 + 4) / panel_Board_horizontal_divid,
+                panel_Board.Height * 2 / panel_Board_vertical_divid
+                );
+            panel_G.Size = new Size(
+                panel_Board.Width / panel_Board_horizontal_divid,
+                panel_Board.Height * (panel_Board_vertical_divid - 3) / panel_Board_vertical_divid
+                );
+            //A
+            panel_A.Location = new Point(
+                panel_Board.Width * ((panel_Board_horizontal_divid - 7) / 2 + 5) / panel_Board_horizontal_divid,
+                panel_Board.Height * 2 / panel_Board_vertical_divid
+                );
+            panel_A.Size = new Size(
+                panel_Board.Width / panel_Board_horizontal_divid,
+                panel_Board.Height * (panel_Board_vertical_divid - 3) / panel_Board_vertical_divid
+                );
+            //B
+            panel_B.Location = new Point(
+                panel_Board.Width * ((panel_Board_horizontal_divid - 7) / 2 + 6) / panel_Board_horizontal_divid,
+                panel_Board.Height * 2 / panel_Board_vertical_divid
+                );
+            panel_B.Size = new Size(
+                panel_Board.Width / panel_Board_horizontal_divid,
+                panel_Board.Height * (panel_Board_vertical_divid - 3) / panel_Board_vertical_divid
+                );
+
+            //黒鍵の位置とサイズ変更
+            //C#
+            panel_Csharp.Location = new Point(
+                panel_Board.Width * (((panel_Board_horizontal_divid - 7) / 2) * 4 + 3) / (panel_Board_horizontal_divid * 4),
+                panel_Board.Height * 2 / panel_Board_vertical_divid
+                );
+            panel_Csharp.Size = new Size(
+                panel_Board.Width / (panel_Board_horizontal_divid * 2),
+                panel_Board.Height * (panel_Board_vertical_divid - 5) / panel_Board_vertical_divid
+                );
+            //D#
+            panel_Dsharp.Location = new Point(
+                panel_Board.Width * (((panel_Board_horizontal_divid - 7) / 2 + 1) * 4 + 3) / (panel_Board_horizontal_divid * 4),
+                panel_Board.Height * 2 / panel_Board_vertical_divid
+                );
+            panel_Dsharp.Size = new Size(
+                panel_Board.Width / (panel_Board_horizontal_divid * 2),
+                panel_Board.Height * (panel_Board_vertical_divid - 5) / panel_Board_vertical_divid
+                );
+            //F#
+            panel_Fsharp.Location = new Point(
+                panel_Board.Width * (((panel_Board_horizontal_divid - 7) / 2 + 3) * 4 + 3) / (panel_Board_horizontal_divid * 4),
+                panel_Board.Height * 2 / panel_Board_vertical_divid
+                );
+            panel_Fsharp.Size = new Size(
+                panel_Board.Width / (panel_Board_horizontal_divid * 2),
+                panel_Board.Height * (panel_Board_vertical_divid - 5) / panel_Board_vertical_divid
+                );
+            //G#
+            panel_Gsharp.Location = new Point(
+                panel_Board.Width * (((panel_Board_horizontal_divid - 7) / 2 + 4) * 4 + 3) / (panel_Board_horizontal_divid * 4),
+                panel_Board.Height * 2 / panel_Board_vertical_divid
+                );
+            panel_Gsharp.Size = new Size(
+                panel_Board.Width / (panel_Board_horizontal_divid * 2),
+                panel_Board.Height * (panel_Board_vertical_divid - 5) / panel_Board_vertical_divid
+                );
+            //A#
+            panel_Asharp.Location = new Point(
+                panel_Board.Width * (((panel_Board_horizontal_divid - 7) / 2 + 5) * 4 + 3) / (panel_Board_horizontal_divid * 4),
+                panel_Board.Height * 2 / panel_Board_vertical_divid
+                );
+            panel_Asharp.Size = new Size(
+                panel_Board.Width / (panel_Board_horizontal_divid * 2),
+                panel_Board.Height * (panel_Board_vertical_divid - 5) / panel_Board_vertical_divid
+                );
         }
     }
 }
